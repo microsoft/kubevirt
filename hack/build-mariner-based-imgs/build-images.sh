447d3d0589abc55dccc9133a61c9ab6a27e8148a
Harshit Gupta <guptaharshit@microsoft.com>
2024-11-22 10:36:22 -0800

    Update build-mariner-based-imgs scripts to AzL3

--- hack/build-mariner-based-imgs/build-images.sh
+++ hack/build-mariner-based-imgs/build-images.sh
@@ -1,23 +1,66 @@
 #!/bin/bash
 
-if [ "$#" -ne 2 ]; then
-    echo "Illegal number of arguments."
-    echo "Correct usage: hack/build-mariner-based-imgs/build-images.sh <vmm> <azure_devops_pat>"
-    echo "Exiting..."
+print_usage() {
+    echo "This is a script to build the KubeVirt images."
+    echo "Before running this script, export the following env variables:"
+    echo "    - DOCKER_PREFIX: prefix to the URL of the container images to be built."
+    echo "                      e.g., export DOCKER_PREFIX=acrafoimages.azurecr.io/kubevirt-mshv/"
+    echo "    - DOCKER_TAG: image tag for the container images to be built."
+    echo "                      e.g., export DOCKER_TAG=20241112-1"
+    echo "    - KUBEVIRT_COMPONENTS: which components of KubeVirt to build. If empty, build all components"
+    echo "                      e.g.,KUBEVIRT_COMPONENTS='virt-launcher virt-controller'"
+    echo ""
+    echo "Script usage: ./build-images.sh -H <hypervisor (ch/qemu)> -P <azure-devops-pat>"
+    echo "                  hypervisor needs to be one of 'ch' and 'qemu'"
+}
+
+if [ -z "$DOCKER_PREFIX" ] || [ -z "$DOCKER_TAG" ]; then
+    echo "Error: This script requires DOCKER_PREFIX and DOCKER_TAG env vars to be set before calling it."
+    print_usage
     exit 1
 fi
 
-vmm=$1
-azureDevopsPat=$2
+while getopts ":hH:P:" option; do
+   case $option in
+      h) # display Help
+          print_usage
+          exit;;
+      H) # Enter the hypervisor
+          hypervisor=$OPTARG
+          echo "hypervisor = $hypervisor"
+          case $hypervisor in
+            ch)
+              ;&
+            qemu)
+              vmm=$hypervisor;;
+            *)
+              echo "Error: Invalid hypervisor argument $hypervisor."
+              echo ""
+              print_usage
+              echo "Exiting..."
+              exit 1
+          esac
+          ;;
+      P) # Personal Access Token to access repos in LSG Azure DevOps Org
+          azureDevopsPat=$OPTARG;;
+     \?) # Invalid option
+          echo "Error: Invalid option"
+          exit;;
+   esac
+done
 
-if [ $vmm == "ch" ]; then
-    :
-elif [ $vmm == "qemu" ]; then
-    :
-else
-    echo "Invalid args: VMM should be one of \"ch\" or \"qemu\". Exiting..."
+if [ -z "$vmm" ]; then
+    echo "Error: Required argument -H <hypervisor> not specified"
+    print_usage
     exit 1
 fi
+if [ -z "$azureDevopsPat" ]; then
+    echo "Error: Required argument -P <azure-devops-pat> not specified"
+    print_usage
+    exit 1
+fi
+
+set -e
 
 SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
 
@@ -33,6 +76,8 @@ for ctr in virt-operator virt-api virt-handler virt-controller; do
       DOCKER_BUILDKIT=1 docker build --build-arg BUILDER_IMAGE=afo-builder:latest \
           -t ${DOCKER_PREFIX}/${ctr}:${DOCKER_TAG} -f $SCRIPT_DIR/Dockerfile-${ctr} .
     fi
+
+    docker system prune -f
 done
 
 # Building the virt-launcher container separately because
@@ -44,4 +89,6 @@ if [[ $KUBEVIRT_COMPONENTS_TO_BUILD == *"$ctr"* ]]; then
         --build-arg AZURE_DEVOPS_PAT=${azureDevopsPat} \
         --build-arg BUILDER_IMAGE=afo-builder:latest \
         -t ${DOCKER_PREFIX}/${ctr}:${DOCKER_TAG} -f $SCRIPT_DIR/Dockerfile-${ctr}-${vmm} .
+
+    docker system prune -f
 fi
