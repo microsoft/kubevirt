88a64f99abc1727122d58f7d81e391463220f84e
Harshit Gupta <guptaharshit@microsoft.com>
2025-02-04 13:01:15 -0800

    Add command to focus run tests

--- hack/run-mshv-tests.sh
+++ hack/run-mshv-tests.sh
@@ -5,7 +5,7 @@ export PULL_POLICY=Always
 export KUBECONFIG=/path/to/kubeconfig
 export DOCKER_PREFIX=[your registry]
 
-
 make cluster-up
 
 make cluster-sync
+./_out/tests/tests.test --ginkgo.v --ginkgo.focus=test_id:3812 -kubeconfig=/tmp/kubeconfig_dir/kubeconfig -config=./tests/default-config.json -kubectl-path=/usr/local/bin/kubectl
