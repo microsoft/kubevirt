60190013f34b30c88501dfbf5ca317fac497790a
Harshit Gupta <guptaharshit@microsoft.com>
2025-06-06 08:32:36 -0700

    Able to build

--- pkg/virt-launcher-common/notify-client/notify_test.go
+++ pkg/virt-launcher-common/notify-client/notify_test.go
@@ -1,9 +1,9 @@
 package eventsclient
 
 import (
-	"go.uber.org/mock/gomock"
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
+	"go.uber.org/mock/gomock"
 
 	"kubevirt.io/kubevirt/pkg/handler-launcher-com/notify/info"
 )
