63c820bbce47d12d9e7e55cf6927bd688995c016
Harshit Gupta <guptaharshit@microsoft.com>
2025-06-09 18:47:43 -0700

    node-labeller.go able to query virsh

---
+++ cmd/node-labeller/node-labeller.go
@@ -0,0 +1,113 @@
+package main
+
+import (
+	"fmt"
+	"os/exec"
+	"strings"
+)
+
+func executeCommand(command string) (string, error) {
+	cmd := exec.Command("bash", "-c", command)
+	output, err := cmd.Output()
+	outputStr := strings.TrimSpace(string(output))
+	if err != nil {
+		return "", fmt.Errorf("failed to execute command %s: %w", command, err)
+	}
+	return outputStr, nil
+}
+
+func queryMachineArchitecture() (string, error) {
+	return executeCommand("uname -m")
+}
+
+func queryKvmMinor() (string, error) {
+	return executeCommand("grep -w 'kvm' /proc/misc | cut -f 1 -d' '")
+}
+
+func main() {
+
+	machine := "q35"
+
+	arch, err := queryMachineArchitecture()
+	if err != nil {
+		fmt.Printf("Error querying machine architecture: %v\n", err)
+		return
+	}
+
+	fmt.Printf("Detected arch=\"%s\"\n", arch)
+
+	if arch == "aarch64" {
+		machine = "virt"
+	} else if arch == "s390x" {
+		machine = "390-ccw-virtio"
+	} else if arch != "x86_64" {
+		// Node labeling cannot proceed for this architecture
+		fmt.Printf("Node labeling cannot proceed for architecture: %s\n", arch)
+		return
+	}
+
+	kvmMinor, err := queryKvmMinor()
+	if err != nil {
+		fmt.Printf("Error querying KVM minor: %v\n", err)
+		return
+	}
+
+	virttype := "qemu"
+
+	_, err = exec.Command("ls", "/dev/kvm").Output()
+	kvmExists := err == nil
+
+	if !kvmExists && kvmMinor != "" {
+		executeCommand("mknod /dev/kvm c 10 " + kvmMinor)
+	}
+
+	_, err = exec.Command("ls", "/dev/kvm").Output()
+	kvmExists = err == nil
+
+	if kvmExists {
+		executeCommand("chmod o+rw /dev/kvm")
+		virttype = "kvm"
+	}
+
+	_, err = exec.Command("ls", "/dev/sev").Output()
+	sevExists := err == nil
+
+	if sevExists {
+		// QEMU requires RW access to query SEV capabilities
+		executeCommand("chmod o+rw /dev/kvm")
+	}
+
+	cmd := exec.Command("virtqemud", "-d")
+	err = cmd.Start()
+	if err != nil {
+		fmt.Printf("Failed to start virtqemud: %v\n", err)
+		return
+	}
+	fmt.Println("virtqemud started in daemon mode")
+
+	domainCapabilitiesOut, err := executeCommand(fmt.Sprintf("virsh domcapabilities --machine %s --arch %s --virttype %s", machine, arch, virttype))
+
+	if err != nil {
+		fmt.Printf("Failed to get domain capabilities: %v\n", err)
+		return
+	}
+	fmt.Println("Domain capabilities retrieved successfully. Length = ", len(domainCapabilitiesOut))
+
+	if arch == "x86_64" || arch == "s390x" {
+		cmd := fmt.Sprintf("virsh domcapabilities --machine %s --arch %s --virttype %s | virsh hypervisor-cpu-baseline --features /dev/stdin --machine %s --arch %s --virttype %s", machine, arch, virttype, machine, arch, virttype)
+		supportedFeatures, err := executeCommand(cmd)
+		if err != nil {
+			fmt.Printf("Failed to get supported features: %v\n", err)
+			return
+		}
+		fmt.Println("Supported features retrieved successfully. Length = ", len(supportedFeatures))
+	}
+
+	nodeCapabilities, err := executeCommand("virsh capabilities")
+	if err != nil {
+		fmt.Printf("Failed to get node capabilities: %v\n", err)
+		return
+	}
+	fmt.Println("Node capabilities retrieved successfully. Length = ", len(nodeCapabilities))
+
+}
