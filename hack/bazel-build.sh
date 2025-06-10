2ffdd9573bd24afd40de634f5e03e28e2bc5b1b9
Harshit Gupta <guptaharshit@microsoft.com>
2025-06-09 19:19:31 -0700

    Make node-labeller-go part of virt-launcher

--- hack/bazel-build.sh
+++ hack/bazel-build.sh
@@ -25,7 +25,6 @@ source hack/config.sh
 
 rm -rf ${CMD_OUT_DIR}
 mkdir -p ${CMD_OUT_DIR}/virtctl
-mkdir -p ${CMD_OUT_DIR}/node-labeller
 mkdir -p ${CMD_OUT_DIR}/dump
 mkdir -p ${CMD_OUT_DIR}/perfscale-audit
 mkdir -p ${CMD_OUT_DIR}/perfscale-load-generator
@@ -66,10 +65,6 @@ bazel run \
 bazel run \
     --config="$(uname -m)" \
     :build-virtctl -- ${CMD_OUT_DIR}/virtctl/virtctl
-
-bazel run \
-    --config="$(uname -m)" \
-    :build-node-labeller -- ${CMD_OUT_DIR}/node-labeller/node-labeller
 
 # Copy kubevirt-passt-binding binary to a reachable place outside of the build container
 bazel run \
