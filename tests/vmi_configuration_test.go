a615db4ea8fad4eb3215e330378de5b6cdd41669
Harshit Gupta <guptaharshit@microsoft.com>
2025-06-11 13:17:59 -0700

    Quarantined test_ids 1677 and 4625 in vmi_configuration_test.go

--- tests/vmi_configuration_test.go
+++ tests/vmi_configuration_test.go
@@ -1132,7 +1132,7 @@ var _ = Describe("[sig-compute]Configurations", decorators.SigCompute, func() {
 				Expect(domXML).To(ContainSubstring("<alias name='channel0'/>"), "Should have guest channel present")
 			})
 
-			It("[test_id:1677]VMI condition should signal agent presence", func() {
+			It("[QUARANTINE][test_id:1677]VMI condition should signal agent presence", func() {
 				agentVMI := prepareAgentVM()
 				getOptions := metav1.GetOptions{}
 
@@ -1147,7 +1147,7 @@ var _ = Describe("[sig-compute]Configurations", decorators.SigCompute, func() {
 
 			})
 
-			It("[test_id:4625]should remove condition when agent is off", func() {
+			It("[QUARANTINE][test_id:4625]should remove condition when agent is off", func() {
 				agentVMI := prepareAgentVM()
 				By("Expecting the VirtualMachineInstance console")
 				Expect(console.LoginToFedora(agentVMI)).To(Succeed())
